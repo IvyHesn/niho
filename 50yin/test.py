08bc37114a5ed4353d346a9eb7370e5cd6bfd4ec
IvyHesn <hsnstudy@126.com>
2015-07-03 15:28:38 +0800

    实现拖动

--- 50yin/test.py
+++ 50yin/test.py
@@ -1,7 +1,7 @@
 # 导入库
 import pygame
 from pygame.locals import *
-
+import PIL
 
 # 初始化Pygame
 pygame.init()
@@ -12,29 +12,42 @@ screen.fill((223, 234, 236))
 pygame.display.set_caption('web_ids')
 
 lo = pygame.image.load('res/ui/lo.png')
+
 drawX = 0
 drawY = 0
-x1 = 0
-y1 = 0
-x2 = 0
-y2 = 0
+x1 = y1 = x2 = y2 = 0
+
+screen.blit(lo, (0, 0))
 
 
-def draw_sceen(drawX, drawY):
-    screen.blit(lo, [drawX, drawY])
-
-while 1:
-    draw_sceen(drawX, drawY)
-    pygame.display.flip()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit(0)
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            x1, y1 = pygame.mouse.get_pos()
-        if event.type == pygame.MOUSEBUTTONUP:
-            x2, y2 = pygame.mouse.get_pos()
+def draw_screen(x1, y1, x2, y2, drawX, drawY):
+    if x2 == y2 == 0:
+        pass
+    else:
         moveX = x2 - x1
         moveY = y2 - y1
-        drawX = drawX + moveX
-        drawY = drawY + moveY
+        drawX += 0
+        drawY += moveY
+        if drawY < height - lo.get_height():
+            drawY = height - lo.get_height()
+        elif drawY > 0:
+            drawY = 0
+        x1 = y1 = x2 = y2 = 0
+        screen.blit(lo, [drawX, drawY])
+
+
+while True:
+    event = pygame.event.wait()
+    draw_screen(x1, y1, x2, y2, drawX, drawY)
+    pygame.display.flip()
+    if event.type == pygame.QUIT:
+        pygame.quit()
+        exit(0)
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+            pygame.quit()
+            exit(0)
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        x1, y1 = pygame.mouse.get_pos()
+    if event.type == pygame.MOUSEBUTTONUP:
+        x2, y2 = pygame.mouse.get_pos()
